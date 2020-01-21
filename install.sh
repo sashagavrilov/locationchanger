4bae3624563839cad73fb15efc19cae706892664
Dennis Muensterer <dennismuensterer@gmail.com>
2020-01-21 08:08:52 +0100

    Enable overwriting config on install

--- install.sh
+++ install.sh
@@ -8,22 +8,33 @@ APP_SUPPORT_DIR="$HOME/.locations" #/Library/Application Support/LocationChanger
 
 sudo -v
 
+echo "Copying script and making it executable"
 sudo cp "$SCRIPT_NAME" "$INSTALL_DIR"
 
 sudo chmod +x ${SCRIPT_NAME}
 
 # generate a default config file if it doesn't exists
-if [ ! -e "${APP_SUPPORT_DIR}/LocationChanger.conf" ]; then
+function copyConfig() {
   mkdir -p "${APP_SUPPORT_DIR}"
+  echo "Copying Config"
   cp LocationChanger.conf "$APP_SUPPORT_DIR"
+}
+if [ -e "${APP_SUPPORT_DIR}/LocationChanger.conf" ]; then
+  echo "Existing config found. Do you want to overwrite it? (y/n)"
+  read reply
+  if [ "$reply" = "y" ]; then
+    copyConfig
+  fi
+else
+  copyConfig
 fi
 
-if [ ! -e "${LAUNCH_AGENTS_DIR}/LocationChanger.plist" ]; then
+if [ ! -e "${LAUNCH_AGENTS_DIR}/LocationChanger.plist" ] || [ "$1" == "-f" ]; then
   mkdir -p "${LAUNCH_AGENTS_DIR}"
+  echo "Copying Plist"
   cp LocationChanger.plist "$LAUNCH_AGENTS_DIR"
 fi
-mkdir -p ${LAUNCH_AGENTS_DIR}
 
 launchctl load ${PLIST_NAME}
 
-echo "Install complete."
+echo "Installation complete."
